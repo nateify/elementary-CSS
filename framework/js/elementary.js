0af255fb0253f905fd67fcbc0f6fbde4b8ca7b8e
nateify <nateify@users.noreply.github.com>
2014-05-24 18:07:21 -0400

    removed old scss files; readded some elements; redesigned form graphics

---
+++ framework/js/elementary.js
@@ -0,0 +1,6 @@
+$(function() {
+	// Checkboxes
+	$('<span class="checkStyle"></span>').insertAfter('input[type="checkbox"]');
+	// Radios
+	$('<span class="radioStyle"></span>').insertAfter('input[type="radio"]');
+});
