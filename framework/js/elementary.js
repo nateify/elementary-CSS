c463c0790819794b337a513b41eb6c357efec2e6
nateify <nateify@users.noreply.github.com>
2014-05-25 17:38:28 -0400

    updated slider/progress bar graphics;replaced png sprite with svg sprite, removed hidpi png;updated elementary.js for slider, progressbar

--- framework/js/elementary.js
+++ framework/js/elementary.js
@@ -3,4 +3,14 @@ $(function() {
 	$('<span class="checkStyle"></span>').insertAfter('input[type="checkbox"]');
 	// Radios
 	$('<span class="radioStyle"></span>').insertAfter('input[type="radio"]');
+	// Slider
+	$(".slider").slider({
+		orientation: "horizontal",
+		start: function() {
+			$("html").css( "cursor", "ew-resize" );
+		},
+		stop: function() {
+			$("html").css( "cursor", "auto" );
+		}
+	});
 });
